9e938aabf4acddf911680fad9d6a6b7041015bfb
Leopoldo Varela <pistolilla58@hotmail.com>
2020-04-24 18:12:12 -0500

    lemmatizer kick off

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 
 @app.before_first_request
 def startup():
-    pass
+    global nlp
+    import spacy
+    nlp = spacy.load('es')
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route("/lematiza/<sent>")
+def lematiza(sent):
+    doc = nlp(sent)
+    return jsonify([token.lemma_ for token in doc])
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
+    #app.run()
